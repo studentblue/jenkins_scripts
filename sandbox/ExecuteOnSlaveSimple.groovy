74db207c17cdbeead74ae79504e2457368a51515
studentblue <e9651415@student.tuwien.ac.at>
2019-03-12 15:09:14 +0100

    added sandbox script

---
+++ sandbox/ExecuteOnSlaveSimple.groovy
@@ -0,0 +1,17 @@
+import hudson.util.RemotingDiagnostics
+import jenkins.model.Jenkins
+
+String agent_name = 'your agent name'
+//groovy script you want executed on an agent
+groovy_script = '''
+println System.getenv("PATH")
+println "uname -a".execute().text
+'''.trim()
+
+String result
+Jenkins.instance.slaves.find { agent ->
+    agent.name == "jenkins-slave1-cpsiot-2018"
+}.with { agent ->
+    result = RemotingDiagnostics.executeGroovy(groovy_script, agent.channel)
+}
+println result
